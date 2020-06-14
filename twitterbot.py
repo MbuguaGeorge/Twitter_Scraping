2e6442edb21086c753dd31a29d4ddcfded214353
MbuguaGeorge <mbuguag026@gmail.com>
2020-06-14 17:03:59 +0300

    initial commit

--- twitterbot.py
+++ twitterbot.py
@@ -1,38 +1,50 @@
 import tweepy
 import sys
+import csv
+import time
 
 
-consumerkey = "BOLQwKdfnW50TlLovyBwcuqLA"
-consumerSecret = "KNPKXK0nO2L7FwTEwK4nHqJJsAX5pCY8pfOTXGO32cZwcUiUgg"
-accessToken = "1154337206875693056-sUaGCmzkcN9jLZr39JY4EyYWzlsNbv"
-accessTokenSecret = "IystYocSsxxBYOykVY5W6mjVdG1KLPrcg3chMqPkg1T85"
+consumerkey = "xxxxxxxxxxxxxxxxxxxxxxx"
+consumerSecret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+accessToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+accessTokenSecret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 auth = tweepy.OAuthHandler(consumerkey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
+
 keyword = input("Enter Tweet To Search: ")
 noOfTweets = int(input("How Many Tweets To Analyze: "))
 
-tweets = tweepy.Cursor(api.search, q= keyword, tweet_mode='extended', include_rts=False).items(noOfTweets)
+tweets = tweepy.Cursor(api.search, q= keyword, tweet_mode="extended").items(noOfTweets)
 
+for tweet in tweets:
+    print(tweet.full_text)
 
 def save_tweet(T):
     T = input("do you want to save tweet? ")
     if T == ("yes") or T == ("Yes") or T == ("YES"):
-        result = input("enter username of tweet to save: ")
-        return result
+        print('saving')
     elif T == ("no") or T == ("No") or T == ("NO"):
         sys.exit()
     else:
         print("invalid")
-        
-UserID='result'
-tweet = api.user_timeline(screen_Name=UserID, count=20)
 
-for info in tweets[3]:
-    print('ID: {}'.format(info.id))
-    print(info.created_at)
-    print(info.full_text)
-    print("\n")
-    
+save_tweet('T')
+
+def extract_tweets(userID):
+    alltweets = []
+    new_tweets = api.user_timeline(screen_name=keyword, count=200, include_rts=False, tweet_mode='extended')
+    alltweets.extend(new_tweets)
+    oldest = alltweets[-1].id - 1
+    outtweets = [
+        [tweet.id_str, tweet.created_at, tweet.full_text.encode("utf-8"), 1 if 'media' in tweet.entities else 0,
+         1 if tweet.entities.get('hashtags') else 0, tweet.retweet_count]
+        for tweet in alltweets]
+    with open('TWEETS.csv', mode='a', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerows(outtweets)
+    pass
+
+extract_tweets('userID')
